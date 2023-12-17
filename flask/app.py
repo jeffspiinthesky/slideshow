a05648c08f6d21123df4bdf5efeedfdc3c336b68
jeffspiinthesky@gmail <jeffspiinthesky@gmail.com>
2023-12-17 12:31:32 -0800

    Corrected settings

--- flask/app.py
+++ flask/app.py
@@ -9,45 +9,53 @@ app = Flask(__name__, instance_relative_config=True)
 UPLOAD_FOLDER = '/flask/static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
-print(conn)
-
-@app.route('/')
-def hello():
-    cursor = conn.cursor()
-    cursor.execute('select p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc')
-    pictures = cursor.fetchall()
-    cursor.execute('select key_value duration from settings where key_name=\'duration\'');
-    duration = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html',pictures=pictures, duration=duration[0][0])
+@app.route('/', methods=['GET'])
+def get_upload_form():
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('select p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc')
+        pictures = cursor.fetchall()
+        cursor.execute('select key_value duration from settings where key_name=\'duration\'');
+        duration = cursor.fetchall()
+        cursor.close()
+        return render_template('index.html',pictures=pictures, duration=duration[0][0])
+    except Exception as e:
+        return json.dumps("Whoops", 500)
+    finally:
+        conn.close()
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # Save filename to the DB
-    cursor = conn.cursor()
-    cursor.execute(f'insert into pictures (name) values (\'{filename}\')')
-    # cursor.lastrowid has the ID of the last added item
-    pic_id = cursor.lastrowid
-    print(f'Added picture ID {pic_id}')
-    # How many pictures do we have?
-    cursor.execute(f'select count(*) from pictures')
-    pic_count = cursor.fetchall()[0][0]
-    # Set this picture as the last item in the list
-    cursor.execute(f'insert into picture_order (picture_id,picture_order) values ({pic_id},{pic_count})')
-    cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
-    conn.commit()
-    cursor.close()
-    return get_pictures()
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Save filename to the DB
+        cursor = conn.cursor()
+        cursor.execute(f'insert into pictures (name) values (\'{filename}\')')
+        # cursor.lastrowid has the ID of the last added item
+        pic_id = cursor.lastrowid
+        print(f'Added picture ID {pic_id}')
+        # How many pictures do we have?
+        cursor.execute(f'select count(*) from pictures')
+        pic_count = cursor.fetchall()[0][0]
+        # Set this picture as the last item in the list
+        cursor.execute(f'insert into picture_order (picture_id,picture_order) values ({pic_id},{pic_count})')
+        cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
+        conn.commit()
+        return get_pictures()
+    except Exception as e:
+        return json.dumps("Whoops", 500)
+    finally:
+        conn.close()
 
 @app.route('/update', methods=['POST'])
 def update():
@@ -56,97 +64,127 @@ def update():
 
 @app.route('/updateduration', methods=['POST'])
 def update_duration():
-    data = request.get_json()
-    duration = data['duration']
-    cursor = conn.cursor()
-    cursor.execute(f'update settings set key_value={duration} where key_name=\'duration\'')
-    cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
-    conn.commit()
-    cursor.close()
-    return Response('OK', 200)
+    try:
+        data = request.get_json()
+        duration = data['duration']
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute(f'update settings set key_value={duration} where key_name=\'duration\'')
+        cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
+        conn.commit()
+        return Response('OK', 200)
+    except Exception as e:
+        return json.dumps("Whoops", 500)
+    finally:
+        conn.close()
            
 @app.route('/reorder', methods=['POST'])
 def reorder():
-    data = request.get_json()
-    print(f'{data}')
-    cursor = conn.cursor()
-    cursor.execute('truncate table picture_order')
-    order = 1
-    for picture_id in data:
-        cursor.execute(f'insert into picture_order (picture_id,picture_order) values ({picture_id}, {order})')
-        order = order + 1
-    cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
-    conn.commit()
-    cursor.close()
-    return Response('OK', 200)
+    try:
+        data = request.get_json()
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('truncate table picture_order')
+        order = 1
+        for picture_id in data:
+            cursor.execute(f'insert into picture_order (picture_id,picture_order) values ({picture_id}, {order})')
+            order = order + 1
+        cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
+        conn.commit()
+        return Response('OK', 200)
+    except Exception as e:
+        return json.dumps("Whoops", 500)
+    finally:
+        conn.close()
 
 @app.route('/pictures', methods=['GET'])
 def get_pictures():
     try:
-        sql = 'select p.id,p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc'
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
         cursor = conn.cursor()
-        cursor.execute(sql)
+        cursor.execute('select p.id,p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc')
         pictures = cursor.fetchall()
         cursor.close()
         bytes = json.dumps(pictures)
         return Response(bytes, mimetype='application/json')
     except Exception as e:
         return Response(e, 500)
+    finally:
+        conn.close()
 
 @app.route('/clearchange', methods=['GET'])
 def reset_change():
-    cursor = conn.cursor()
-    cursor.execute('update settings set key_value=0 where key_name=\'changed\'')
-    conn.commit()
-    cursor.close()
-    return Response('OK', 200)
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('update settings set key_value=0 where key_name=\'changed\'')
+        conn.commit()
+        return Response('OK', 200)
+    except Exception as e:
+        return Response(e, 500)
+    finally:
+        conn.close()
 
 @app.route('/getchange', methods=['GET'])
 def get_change():
-    cursor = conn.cursor()
-    cursor.execute('select key_value from settings where key_name=\'changed\'')
-    changed = cursor.fetchall()
-    print(f'Changed: {changed}')
-    bytes = json.dumps(changed[0])
-    cursor.close()
-    return Response(bytes, mimetype='application/json')
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('select key_value from settings where key_name=\'changed\'')
+        changed = cursor.fetchall()
+        bytes = json.dumps(changed[0])
+        cursor.close()
+        return Response(bytes, mimetype='application/json')
+    except Exception as e:
+        return Response(e, 500)
+    finally:
+        conn.close()
 
 @app.route('/getduration', methods=['GET'])
 def get_duration():
-    cursor = conn.cursor()
-    cursor.execute('select key_value from settings where key_name=\'duration\'')
-    duration = cursor.fetchall()
-    print(f'Duration: {duration}')
-    bytes = json.dumps(duration[0])
-    return Response(bytes, mimetype='application/json')
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('select key_value from settings where key_name=\'duration\'')
+        duration = cursor.fetchall()
+        bytes = json.dumps(duration[0])
+        cursor.close()
+        return Response(bytes, mimetype='application/json')
+    except Exception as e:
+        return Response(e, 500)
+    finally:
+        conn.close()
 
 @app.route('/slideshow', methods=['GET'])
 def render_slideshow():
-    sql = 'select p.id,p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc'
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    pictures = cursor.fetchall()
-    cursor.close()
-    return render_template('slideshow.html', pictures=pictures)
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        cursor = conn.cursor()
+        cursor.execute('select p.id,p.name,o.picture_order from pictures p inner join picture_order o on p.id=o.picture_id order by o.picture_order asc')
+        pictures = cursor.fetchall()
+        cursor.close()
+        return render_template('slideshow.html', pictures=pictures)
+    except Exception as e:
+        return Response(e, 500)
+    finally:
+        conn.close()
 
 @app.route('/delpicture', methods=['POST'])
 def delete_image():
-    data = request.get_json()
-    print(f'{data["id"]}')
-    cursor = conn.cursor()
-    cursor.execute(f'delete from picture_order where picture_id={data["id"]};')
-    cursor.execute(f'delete from pictures where id={data["id"]}')
-    cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
-    conn.commit()
-    cursor.close()
-    return Response("OK", 200)
-
-def close_db():
-    print('Closing DB connection')
-    conn.close()
+    try:
+        conn = mysql.connector.connect(user='root', password='rootroot', host='slideshowdb', port='3306', database='pictureframe')
+        data = request.get_json()
+        cursor = conn.cursor()
+        cursor.execute(f'delete from picture_order where picture_id={data["id"]};')
+        cursor.execute(f'delete from pictures where id={data["id"]}')
+        cursor.execute(f'update settings set key_value=1 where key_name=\'changed\'')
+        conn.commit()
+        cursor.close()
+        return Response("OK", 200)
+    except Exception as e:
+        return Response(e, 500)
+    finally:
+        conn.close()
 
 if __name__ == "__main__":
     app.run()
-atexit.register(close_db)
-
-
